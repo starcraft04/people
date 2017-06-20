67cc23d9f62346dae30cc94a14f110259ef8cf20
John Dauphinais <jdauphinais@gmail.com>
2017-06-20 00:22:52 -0700

    Added protection of nice_artisan can only be accessed by admin user

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,5 +51,6 @@ class Kernel extends HttpKernel
         'role' => \Zizaco\Entrust\Middleware\EntrustRole::class,
         'permission' => \Zizaco\Entrust\Middleware\EntrustPermission::class,
         'ability' => \Zizaco\Entrust\Middleware\EntrustAbility::class,
+        'nice_artisan' => \App\Http\Middleware\NiceArtisan::class,
     ];
 }
