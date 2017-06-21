3c967c27fb763eb642840bb4f30c17da5a5b81d5
John Dauphinais <jdauphinais@gmail.com>
2017-06-21 12:33:16 -0700

    Removed google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -107,17 +107,6 @@
     <script src="{{ asset('/plugins/gentelella/vendors/nprogress/nprogress.js') }}" type="text/javascript"></script>
     <!-- Custom Theme Scripts -->
     <script src="{{ asset('/plugins/gentelella/build/js/custom.min.js') }}" type="text/javascript"></script>
-    <!-- Google Analytics -->
-    <script type="text/javascript">
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-23581568-13', 'auto');
-      ga('send', 'pageview');
-
-    </script>
 
     @yield('scriptsrc')
     @yield('script')
