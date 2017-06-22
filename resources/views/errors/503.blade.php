b8500f0b9137dd98221d4876c0ce862d7e52798e
John Dauphinais <jdauphinais@gmail.com>
2017-06-22 04:40:53 -0700

    Changed the down for maintenance message

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -40,7 +40,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Be right back.</div>
+                <div class="title">Down for maintenance on the Database.</div>
             </div>
         </div>
     </body>
