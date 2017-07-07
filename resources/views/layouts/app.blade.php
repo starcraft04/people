4148d10d4435178c3c4548659bab0e10d1e96d63
John Dauphinais <jdauphinais@gmail.com>
2017-07-07 13:29:55 -0700

    Reassigned the ie8 if else

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,16 @@
   <!-- Custom Theme Style -->
   <link href="{{ asset('/plugins/gentelella/build/css/custom.min.css') }}" rel="stylesheet">
 
+  <!--[if lt IE 9]>
+  <script src="../assets/js/ie8-responsive-file-warning.js"></script>
+  <![endif]-->
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
   @yield('style')
 
 
