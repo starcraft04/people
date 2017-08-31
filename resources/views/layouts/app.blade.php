2b3fce484015e3a3d38d8890f371d81fa9f1c2c2
John Dauphinais <jdauphinais@gmail.com>
2017-08-31 05:59:39 -0700

    Added new datatables and export to excel only visible

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,26 @@
   <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
+
+  <!-- All script sources -->
+    <!-- jQuery -->
+    <script src="{{ asset('/plugins/gentelella/vendors/jquery/dist/jquery.min.js') }}" type="text/javascript"></script>
+    <!-- Bootstrap -->
+    <script src="{{ asset('/plugins/gentelella/vendors/bootstrap/dist/js/bootstrap.min.js') }}" type="text/javascript"></script>
+    <!-- FastClick -->
+    <script src="{{ asset('/plugins/gentelella/vendors/fastclick/lib/fastclick.js') }}" type="text/javascript"></script>
+    <!-- NProgress -->
+    <script src="{{ asset('/plugins/gentelella/vendors/nprogress/nprogress.js') }}" type="text/javascript"></script>
+    <!-- Custom Theme Scripts -->
+    <script src="{{ asset('/plugins/gentelella/build/js/custom.min.js') }}" type="text/javascript"></script>
+    <!-- JS cookie -->
+    <script src="{{ asset('/plugins/jscookie/js.cookie.js') }}" type="text/javascript"></script>
+    @yield('scriptsrc')
+    <!-- END All script sources -->
+    <!-- All scripts -->
+    @yield('script')
+
+    
 </head>
 
 
@@ -116,46 +136,28 @@
 
       </div>
     </div>
-    
-    <!-- All script sources -->
-    <!-- jQuery -->
-    <script src="{{ asset('/plugins/gentelella/vendors/jquery/dist/jquery.min.js') }}" type="text/javascript"></script>
-    <!-- Bootstrap -->
-    <script src="{{ asset('/plugins/gentelella/vendors/bootstrap/dist/js/bootstrap.min.js') }}" type="text/javascript"></script>
-    <!-- FastClick -->
-    <script src="{{ asset('/plugins/gentelella/vendors/fastclick/lib/fastclick.js') }}" type="text/javascript"></script>
-    <!-- NProgress -->
-    <script src="{{ asset('/plugins/gentelella/vendors/nprogress/nprogress.js') }}" type="text/javascript"></script>
-    <!-- Custom Theme Scripts -->
-    <script src="{{ asset('/plugins/gentelella/build/js/custom.min.js') }}" type="text/javascript"></script>
-    <!-- JS cookie -->
-    <script src="{{ asset('/plugins/jscookie/js.cookie.js') }}" type="text/javascript"></script>
-    @yield('scriptsrc')
-    <!-- END All script sources -->
-    <!-- All scripts -->
-    @yield('script')
+
     <script>
+      $(document).ready(function() {
 
-    $(document).ready(function() {
+        @if ($message = Session::get('success'))
+        $('#flash-success').delay(2000).queue(function () {
+            $(this).addClass('animated flipOutX')
+        });
+        @endif
+        @if ($message = Session::get('error'))
+        $('#flash-error').delay(2000).queue(function () {
+            $(this).addClass('animated flipOutX')
+        });
+        @endif
 
-      @if ($message = Session::get('success'))
-      $('#flash-success').delay(2000).queue(function () {
-          $(this).addClass('animated flipOutX')
+        $('#logout').on('click', function () {
+            Cookies.remove('year');
+            Cookies.remove('manager');
+            Cookies.remove('user');
+            window.location.href = "{{ route('auth.logout') }}";
+        });
       });
-      @endif
-      @if ($message = Session::get('error'))
-      $('#flash-error').delay(2000).queue(function () {
-          $(this).addClass('animated flipOutX')
-      });
-      @endif
-
-      $('#logout').on('click', function () {
-          Cookies.remove('year');
-          Cookies.remove('manager');
-          Cookies.remove('user');
-          window.location.href = "{{ route('auth.logout') }}";
-      });
-    });
     </script>
     <!-- END All scripts -->
 
