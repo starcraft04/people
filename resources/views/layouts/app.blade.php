a53b6fa4a9244377532f4236e6d6015f82523e23
John Dauphinais <jdauphinais@gmail.com>
2017-10-19 09:11:10 -0700

    Changed name and logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
         <div class="col-md-3 left_col menu_fixed">
           <div class="left_col scroll-view">
             <div class="navbar nav_title" style="border: 0;">
-              <a href="{!!route('home')!!}" class="site_title"><i class="fa fa-paw"></i> <span>People</span></a>
+              <a href="{!!route('home')!!}" class="site_title"><img src="{{ asset("/img/dolphin-logo.png") }}"><span>Dolphin</span></a>
             </div>
 
             <div class="clearfix"></div>
