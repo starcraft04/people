ea81e83deafbe04eefa936a6fd61ea8152e3909c
Laravel Shift <shift@laravelshift.com>
2020-05-11 15:20:38 +0000

    Shift core files

--- app/User.php
+++ app/User.php
@@ -2,11 +2,13 @@
 
 namespace App;
 
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Zizaco\Entrust\Traits\EntrustUserTrait;
 
 class User extends Authenticatable
 {
+    use Notifiable;
     use EntrustUserTrait; // add this trait to your user model
 
     protected $table = 'users';
