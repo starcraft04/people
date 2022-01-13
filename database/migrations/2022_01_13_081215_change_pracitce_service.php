9691c3931e6fa394a9355088a0ef69c95bd3aeaa
minamisak <mina.m.misak@gmail.com>
2022-01-13 09:26:07 +0200

    change in database

---
+++ database/migrations/2022_01_13_081215_change_pracitce_service.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangePracitceService extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+     DB::statement("UPDATE users SET domain = 'Service Assurance and Performance' WHERE domain = 'IT Assurance and Performance';");
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        DB::statement("UPDATE users SET domain = 'IT Assurance and Performance' WHERE domain = 'Service Assurance and Performance';");
+    }
+}
