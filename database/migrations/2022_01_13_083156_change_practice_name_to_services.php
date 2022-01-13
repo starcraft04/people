15af0418722e39ffe29732a2702786ff83aedada
minamisak <mina.m.misak@gmail.com>
2022-01-13 09:34:44 +0200

    add new migration to change practice name

---
+++ database/migrations/2022_01_13_083156_change_practice_name_to_services.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangePracticeNameToServices extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+        DB::statement("UPDATE users SET domain = 'IT Assurance and Performance' WHERE domain = 'Service Assurance and Performance';");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        
+     DB::statement("UPDATE users SET domain = 'Service Assurance and Performance' WHERE domain = 'IT Assurance and Performance';");
+    }
+}
