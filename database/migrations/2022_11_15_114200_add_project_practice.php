e193018a52e34160d1b100545b33bb10c229770b
Mina Misak <mina.m.misak@gmail.com>
2022-11-15 12:44:16 +0200

    project migrations

---
+++ database/migrations/2022_11_15_114200_add_project_practice.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddProjectPractice extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('projects', function (Blueprint $table) {
+            $table->string('project_practice');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
